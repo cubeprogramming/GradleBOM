53d4a87265900265cab98af1e8a8ef7a9fc88021
Dalibor Blazevic <dalibor.blazevic@sysco.no>
2020-02-01 20:32:59 +0100

    Added plugins.properties

--- BOMConsumingProject/settings.gradle.kts
+++ BOMConsumingProject/settings.gradle.kts
@@ -1,12 +1,16 @@
+import java.io.InputStreamReader
+import java.io.BufferedReader
+import java.util.Properties
+import java.net.URL
+
 //rootProject.name = "BOMConsumingProject"
 pluginManagement {
     //Defining access to remote properties file
-    val prop = java.util.Properties()
-    val propertyResource = java.net.URL("https://raw.githubusercontent.com/cubeprogramming/GradleBOM/master/MavenBOMPublisher/gradle.properties")
-    prop.load(java.io.InputStreamReader(propertyResource.openStream()))
+    val prop = Properties()
+    val propertyResource = URL("https://raw.githubusercontent.com/cubeprogramming/GradleBOM/master/MavenBOMPublisher/gradle.properties")
+    prop.load(InputStreamReader(propertyResource.openStream()))
     println("Value is =" + prop.getProperty("kotlinPluginVersion"))
 
-    val kotlinPluginVersion: String by settings
     plugins {
         kotlin("jvm") version prop.getProperty("kotlinPluginVersion")
         id("io.spring.dependency-management") version "1.0.9.RELEASE"
@@ -16,9 +20,14 @@ pluginManagement {
 
 gradle.allprojects{
     repositories {
-        //    mavenLocal()
+        //Define repository list loader
+        val repositoryListRes = URL("https://raw.githubusercontent.com/cubeprogramming/GradleBOM/master/MavenBOMPublisher/repositoryList")
+        val repositoryLines = BufferedReader(InputStreamReader(repositoryListRes.openStream()))
+
         mavenCentral()
-        maven(url = uri("http://localhost:8081/repository/maven-snapshots/"))
+        repositoryLines.forEachLine {
+            maven(url = uri(it))
+        }
     }
 }
 
